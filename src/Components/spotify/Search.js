11f87431376e5096719f40bd7d142d03413e6d7b
Andy Nguyen <brobromario@hotmail.com>
2020-04-01 05:44:24 -0400

    redo artist suggestion

--- src/Components/spotify/Search.js
+++ src/Components/spotify/Search.js
@@ -1,50 +1,68 @@
 import React, { Component } from "react";
 import {filterArtistByName} from "./filterArtist";
-class Search extends Component {
-  state = { artistQuery: "", listOfArtist: [] };
+// import 'antd/dist/antd.css';
 
-  updateArtistQuery = event => {
+class Search extends Component {
+  state = { artistQuery: "", listOfArtist: [], hidden: true};
+
+  fetchArtistList = (event) => {
     const artistQuery = event.target.value;
-    this.setState({ listOfArtist : filterArtistByName(artistQuery), artistQuery });
+    const listOfArtist =  filterArtistByName(artistQuery);
+    this.setState({ listOfArtist, artistQuery,hidden: false });
   };
 
+  //DNW!!
   handleKeyPress = event => {
     if (event.key === "Enter") {
       this.searchArtist();
     }
   };
 
+  setArtist = (artistQuery) =>  this.setState({ artistQuery });
+
   searchArtist = () => {
+    this.setState({ hidden : true })
     this.props.searchArtist(this.state.artistQuery);
   };
 
-  /*
-  this.props.noResultsFound !== "" && (this.props.noResultsFound === "false") ? "artist_search_success" : "artist_search_error" 
-
-  */
-
+//this.state.
 
   render() {
+
+    // const options =  (this.state.listOfArtist.slice(0,7)).map((artist, key) =>
+    //   <option key={key} value={artist}> {artist} </option> 
+    // );
+
+    const artists = (<ul className="select-list-group__list" data-toggle="false">
+                        {(this.state.listOfArtist.slice(0,7)).map((artist, key) =>
+                              <li className="select-list-group__list-item" key={key} data-display="true" data-highlight="false" onClick={() => this.setArtist(artist) }> {artist} </li> 
+                          )}
+                    </ul>)
+    // {typeof(artist)}
     return (
       <div className="artist__search__section">
         <div className="search__artist" id={this.props.noResultsFound.className}>
           <div className = "spotify__input__section">
-        
-            <input
+          <input
               list="artists"
               className= "search__input"
               type="search"
-              onChange={this.updateArtistQuery}
+              onChange={this.fetchArtistList}
               onKeyPress={this.handleKeyPress}
               placeholder={" Search for an Artist"}
-              required/>
-         
-          <datalist id="artists"  className= "search__datalist"> 
-            { this.state.listOfArtist.slice(0,7).map((artist, key) =>
-               <option id="select__artist__option" key={key} value={artist}> {artist} </option>) 
-            }
-          </datalist>
-         
+              required
+              value={this.state.artistQuery}/>
+         {(this.state.listOfArtist.length > 0 &&  this.state.hidden === false) && 
+              <div className="artist__suggestion"> 
+                <button className="close__suggestion" onClick={() => this.setState({ hidden: true }) }> X </button>
+                {artists}
+              </div>
+          }
+          {/* <datalist id="artists"  className= "search__datalist"> 
+           <select id="artists" className= "search__select">
+            { options }
+            </select>
+          </datalist> */}
           </div>
           <button
               className={`search_button ${this.props.noResultsFound.className}`}
@@ -64,3 +82,4 @@ class Search extends Component {
 }
 
 export default Search;
+// {artist is type array???? }
