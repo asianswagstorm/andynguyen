517f343a820ae19d1461e43401c38f5669bcc140
Andy Nguyen <brobromario@hotmail.com>
2020-04-01 19:20:41 -0400

    artist suggestion completed

--- src/Components/spotify/Search.js
+++ src/Components/spotify/Search.js
@@ -1,68 +1,75 @@
 import React, { Component } from "react";
 import {filterArtistByName} from "./filterArtist";
 // import 'antd/dist/antd.css';
+const listLimit = 6;
 
 class Search extends Component {
-  state = { artistQuery: "", listOfArtist: [], hidden: true};
+  state = { artistQuery: "", listOfArtist: [], hidden: true, currentSelected: -1}; //, hoveredIndex : -1
 
   fetchArtistList = (event) => {
     const artistQuery = event.target.value;
     const listOfArtist =  filterArtistByName(artistQuery);
-    this.setState({ listOfArtist, artistQuery,hidden: false });
+    this.setState({ listOfArtist, artistQuery,hidden : false, currentSelected: -1 });
   };
 
-  //DNW!!
   handleKeyPress = event => {
     if (event.key === "Enter") {
       this.searchArtist();
     }
   };
 
-  setArtist = (artistQuery) =>  this.setState({ artistQuery });
+  selectArtist = (artistQuery, currentSelected) =>  this.setState({ artistQuery, hidden : true ,currentSelected});
+  hoverArtist = (artistQuery, currentSelected) =>  this.setState({ artistQuery ,currentSelected});
 
   searchArtist = () => {
-    this.setState({ hidden : true })
+    this.setState({ hidden : true , currentSelected : -1 })
     this.props.searchArtist(this.state.artistQuery);
   };
 
-//this.state.
+  handleKeyDown = (event) => {
+      //event.keyCode (down) 40 , (up) 38
+     if (this.state.hidden === false && (event.keyCode === 40 || event.keyCode === 38) ) {
+          const currentKey = event.keyCode === 40 ? (this.state.currentSelected === listLimit ? 0 : this.state.currentSelected + 1) : 
+          (event.keyCode === 38  ? ((this.state.currentSelected <= 0) ? listLimit : this.state.currentSelected - 1) : this.state.currentSelected);
+          this.setState({
+            currentSelected : currentKey,
+            artistQuery : this.state.listOfArtist[currentKey]
+          });
+    }
+  };
 
   render() {
 
-    // const options =  (this.state.listOfArtist.slice(0,7)).map((artist, key) =>
-    //   <option key={key} value={artist}> {artist} </option> 
-    // );
-
-    const artists = (<ul className="select-list-group__list" data-toggle="false">
-                        {(this.state.listOfArtist.slice(0,7)).map((artist, key) =>
-                              <li className="select-list-group__list-item" key={key} data-display="true" data-highlight="false" onClick={() => this.setArtist(artist) }> {artist} </li> 
+    const artists = (<ul className="select-list-group__list">
+                        {(this.state.listOfArtist.slice(0, listLimit+1)).map((artist, key) =>
+                              <li className="select-list-group__list-item" id= {key === this.state.currentSelected ? "list__item__active" : "list__item__inactive"} key={key} onClick={() => this.selectArtist(artist, key)}
+                              onMouseEnter = {() => this.hoverArtist(artist,key)} >
+                                  {artist} 
+                              </li> 
                           )}
                     </ul>)
-    // {typeof(artist)}
     return (
       <div className="artist__search__section">
         <div className="search__artist" id={this.props.noResultsFound.className}>
           <div className = "spotify__input__section">
           <input
+              onKeyDown = {this.handleKeyDown}
               list="artists"
               className= "search__input"
               type="search"
               onChange={this.fetchArtistList}
               onKeyPress={this.handleKeyPress}
+              onFocus = { () => this.setState({ hidden : true, currentSelected : -1 })}
               placeholder={" Search for an Artist"}
               required
               value={this.state.artistQuery}/>
+
          {(this.state.listOfArtist.length > 0 &&  this.state.hidden === false) && 
               <div className="artist__suggestion"> 
-                <button className="close__suggestion" onClick={() => this.setState({ hidden: true }) }> X </button>
                 {artists}
               </div>
           }
-          {/* <datalist id="artists"  className= "search__datalist"> 
-           <select id="artists" className= "search__select">
-            { options }
-            </select>
-          </datalist> */}
+       
           </div>
           <button
               className={`search_button ${this.props.noResultsFound.className}`}
