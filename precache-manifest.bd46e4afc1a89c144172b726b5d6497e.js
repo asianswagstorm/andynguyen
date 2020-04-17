b7c52efe2dfd3f077f4f1cf8ca0127fa13ddbf30
Andy Nguyen <brobromario@hotmail.com>
2020-04-17 12:31:27 -0400

    Updates

---
+++ precache-manifest.bd46e4afc1a89c144172b726b5d6497e.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cce7709a674282766832cff8a3644d74",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f962ed5f300f8aa72830",
+    "url": "/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "a02261e477c2a08a2c22",
+    "url": "/static/css/main.bb54f857.chunk.css"
+  },
+  {
+    "revision": "f962ed5f300f8aa72830",
+    "url": "/static/js/2.5b3492f0.chunk.js"
+  },
+  {
+    "revision": "0e49f155131f58fb6f6b9ad3012d14d2",
+    "url": "/static/js/2.5b3492f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a02261e477c2a08a2c22",
+    "url": "/static/js/main.84f61406.chunk.js"
+  },
+  {
+    "revision": "1e4f92fee09803f0a82d",
+    "url": "/static/js/runtime-main.240c0830.js"
+  },
+  {
+    "revision": "f935f7aa56e2ffe3d4d9a75180647d98",
+    "url": "/static/media/azure.f935f7aa.png"
+  },
+  {
+    "revision": "c0e4e8521866215577013dc4c3d8d7f0",
+    "url": "/static/media/banner.c0e4e852.jpg"
+  },
+  {
+    "revision": "c8d155671920e73257deb84e263d6083",
+    "url": "/static/media/canada.c8d15567.topojson"
+  },
+  {
+    "revision": "82f8cdf8530bde98c33877d1d055259d",
+    "url": "/static/media/django.82f8cdf8.png"
+  },
+  {
+    "revision": "1dc35d25e61d819a9c357074014867ab",
+    "url": "/static/media/fontawesome-webfont.1dc35d25.ttf"
+  },
+  {
+    "revision": "24c601e721ebd8279d38e2cfa0d01bc6",
+    "url": "/static/media/fontawesome-webfont.24c601e7.svg"
+  },
+  {
+    "revision": "25a32416abee198dd821b0b17a198a8f",
+    "url": "/static/media/fontawesome-webfont.25a32416.eot"
+  },
+  {
+    "revision": "c8ddf1e5e5bf3682bc7bebf30f394148",
+    "url": "/static/media/fontawesome-webfont.c8ddf1e5.woff"
+  },
+  {
+    "revision": "e6cf7c6ec7c2d6f670ae9d762604cb0b",
+    "url": "/static/media/fontawesome-webfont.e6cf7c6e.woff2"
+  },
+  {
+    "revision": "6c4d02dc7cc1e1118e4592c31d6c5763",
+    "url": "/static/media/montreal.6c4d02dc.topojson"
+  },
+  {
+    "revision": "c3ba82a536fe4983dad4c06b578dcd5d",
+    "url": "/static/media/react.c3ba82a5.png"
+  },
+  {
+    "revision": "c8f44df3b0bdbe3addb48058ed44c088",
+    "url": "/static/media/redux.c8f44df3.png"
+  },
+  {
+    "revision": "ab6bd65370ea8d395759afec3478c7b4",
+    "url": "/static/media/world.ab6bd653.topojson"
+  }
+]);
